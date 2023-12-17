1d8ac892fa63a00ec9ef96c77285dd340c319c95
Diogo Marins <diogodsfmarins@gmail.com>
2023-12-17 17:47:14 -0300

    feat: conditionals

---
+++ foundation/14-conditionals/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 7%2 == 0 || 8%2 == 0 {
+		fmt.Println("either 8 or 7 are even")
+	}
+
+	if 5 > 0 && 5 < 10 {
+		fmt.Println("5 is valid")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
