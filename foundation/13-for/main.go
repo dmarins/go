34ff0e9bca139528a8bdf911da62bc04b8d1aa99
Diogo Marins <diogodsfmarins@gmail.com>
2023-12-17 17:35:27 -0300

    feat: for

---
+++ foundation/13-for/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
+
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
+
+    for {
+        fmt.Println("loop")
+        break
+    }
+
+    for n := 0; n <= 5; n++ {
+        if n%2 == 0 {
+            continue
+        }
+        fmt.Println(n)
+    }
+
+	nums := []int{2, 3, 4}
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+}
