5b50cc6452938ef9b49b247713b279851f257038
Diogo Marins <diogodsfmarins@gmail.com>
2024-01-01 20:36:06 -0300

    feat: read a product in db

---
+++ db/read/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    "",
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	product, err := selectProduct(db, "c93d4bee-3efa-4bd4-a9e4-127bd4873380")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(product)
+}
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	product := Product{}
+
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
