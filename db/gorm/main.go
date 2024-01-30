7b31130059bd47dd23c300fea1f6ef6ef1df9782
dmarins <diogodsfmarins@gmail.com>
2024-01-29 21:02:46 -0300

    feat: insert, first and find items

--- db/gorm/main.go
+++ db/gorm/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,12 +13,32 @@ type Product struct {
 	Price float64
 }
 
+// docker-compose up -d
+// docker-compose exec mysql bash
+// mysql -u root -p go
+
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/go"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
 	db.AutoMigrate(&Product{})
+
+	// db.Create(&Product{
+	// 	Name:  "Mouse",
+	// 	Price: 100.00,
+	// })
+
+	// var product Product
+	// db.First(&product, 1)
+	// db.First(&product, "name = ?", "notebook")
+	// fmt.Println(product)
+
+	var products []Product
+	db.Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
+	}
 }
